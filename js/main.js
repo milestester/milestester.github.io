a528e1639a32fe20b8532a1a39181b879546edc0
Miles Tester <milesdeant@gmail.com>
2014-05-08 21:28:10 -0700

    Updated Whole Site
    Fully Updated, Responsive Layout

--- js/main.js
+++ js/main.js
@@ -20,15 +20,4 @@ $(document).ready(function() {
     })
 
 });
-  
-$(function() {
-  $("#bars li .bar").each( function( key, bar ) {
-    var percentage = $(this).data('percentage');
-
-    $(this).animate({
-      'height' : percentage + '%'
-    }, 1000);
-  });
-});
-
 
